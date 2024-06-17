1521c6e1bbb283dbfb320fdece590a7560664519
Abiezaptr <abieza.eresha@gmail.com>
2024-06-17 17:21:20 +0700

    update view detail konsultasi

--- application/views/partials/other/header.php
+++ application/views/partials/other/header.php
@@ -23,6 +23,7 @@
     <!-- Favicon -->
     <!-- --------------------------------------------------- -->
     <link rel="shortcut icon" type="image/png" href="<?= base_url('assets') ?>/img/favicon.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- --------------------------------------------------- -->
     <!-- Owl Carousel -->
